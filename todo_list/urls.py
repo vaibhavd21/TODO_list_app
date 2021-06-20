463c3dbf57530a93960b1595689b5883bdde2cd5
vaibhavd <vaibhavdubewar28800@gmail.com>
2021-06-20 19:32:11 +0530

    change in urls of project

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
