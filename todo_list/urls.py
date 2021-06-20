a3653b057c9a2543a678effe21256c5a785f258a
vaibhavd <vaibhavdubewar28800@gmail.com>
2021-06-20 18:12:26 +0530

    second commit

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,8 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('todo_list_app.urls'))
+    path('',include('todo_list_app.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
